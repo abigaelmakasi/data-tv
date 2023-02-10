c7f2a1c6da7fdf733daefaecb6f3a9f21771e48d
unknown <abigaelmakasi160@gmail.com>
2023-02-10 16:11:27 +0100

    positionnement de l'icone

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,96 +16,107 @@ class _HomeScreenState extends State<HomeScreen> {
             title: const Text("data tv"),
           ),
           body: Center(
-            child:  Column(
-            children: [
-              Row(
-                children: [
-                  Image.asset(
-                    "assets/images/img2.PNG",
-                    width: 100,
-                    height: 100,
-                  ),
-                  const Expanded(
-                    child: Text(
-                    ("bienvenue sur notre chaine data tv,nous vous offrons les meilleure programme "),
-                    style: TextStyle(
-                      fontSize: 14,
+            child: Column(
+              children: [
+                Container(
+                    height: 200,
+                    width: double.infinity,
+                    color: Colors.black,
+                    padding: const EdgeInsets.all(20),
+                    margin: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Center(
+                          child: Stack(
+                            children: const [
+                              Icon(
+                                Icons.play_arrow,
+                                color: Colors.red,
+                                size: 130.0,
+                              ),
+                              
+                            ],
+                          ),
+                        ),
+                        const Align(
+                                alignment: Alignment.bottomLeft,
+                                child: Text("En direct",style: TextStyle(color: Colors.white, fontSize: 14),),
+                             
+                              ),
+                      ],
+                    )),
+               Flexible(child: 
+                Column(
+                  children: [
+                    Row(
+                  children: [
+                    Image.asset(
+                      "assets/images/img2.PNG",
+                      width: 100,
+                      height: 100,
                     ),
-                  ),
-                  )
-                 
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset(
-                    "assets/images/img2.PNG",
-                    width: 100,
-                    height: 100,
-                  ),
-
-                  const Expanded(
-                    child:  Text(
-                    ("l'arriver du pape dens la ville de kinshasa a diminuer sensiblement les embouteillage"),
-                    style: TextStyle(
-                      fontSize: 14,
+                    const Expanded(
+                      child: Text(
+                        ("l'arriver du pape dens la ville de kinshasa a diminuer sensiblement les embouteillage"),
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      "assets/images/img2.PNG",
+                      width: 100,
+                      height: 100,
                     ),
-                
-                  ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset(
-                    "assets/images/img2.PNG",
-                    width: 100,
-                    height: 100,
-                  ),
-                   const Expanded(
-                    child:Text(
+                    const Expanded(
+                        child: Text(
                       ("l'eglise catholique refuse le mariage  homo sexuel "),
                       style: TextStyle(
                         fontSize: 14,
                       ),
-                    )
-                     )
-               
-                ],
-                
-              ),
-              
-            ],
-          ),
+                    ))
+                  ],
+                  
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      "assets/images/img2.PNG",
+                      width: 100,
+                      height: 100,
+                    ),
+                    const Expanded(
+                        child: Text(
+                      ("l'eglise catholique refuse le mariage  homo sexuel "),
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ))
+                  ],
+                  
+                ),
+                  ],
+                ))
+              ],
             ),
-          
+          ),
           bottomNavigationBar: BottomNavigationBar(
             selectedItemColor: Colors.pink,
             unselectedItemColor: Colors.black,
             items: const [
               BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: ("accuiel")
-                ),
-                BottomNavigationBarItem(
-                icon: Icon(Icons.calendar_month),
-                label: ("calendrier")
-                ),
-                BottomNavigationBarItem(
-                icon: Icon(Icons.add),
-                label: ("ajout")
-                ),
-
-                BottomNavigationBarItem(
-                icon: Icon(Icons.play_arrow),
-                label: ("ajout")
-                ),
-                
+                  icon: Icon(Icons.home), label: ("accuiel")),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.calendar_month), label: ("calendrier")),
+              BottomNavigationBarItem(icon: Icon(Icons.add), label: ("ajout")),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.play_arrow), label: ("ajout")),
             ],
-            
-            
-          )
-          ),
+          )),
     );
   }
 }

e86aec9277f5d4e147a520550490d488f965a429
DMiyalu <dieudonnemiyalu6@gmail.com>
2023-02-24 16:14:17 +0100

    quick fix

--- lib/common/platform/connectivity_manager.dart
+++ lib/common/platform/connectivity_manager.dart
@@ -1,14 +1,14 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
+// import 'package:connectivity_plus/connectivity_plus.dart';
 
-class ConnectivityManager {
-  static get isConnected async {
-    ConnectivityResult connectivityResult =
-        await (Connectivity().checkConnectivity());
-    if (connectivityResult == ConnectivityResult.mobile) {
-      return true;
-    } else if (connectivityResult == ConnectivityResult.wifi) {
-      return true;
-    }
-    return false;
-  }
-}
+// class ConnectivityManager {
+//   static get isConnected async {
+//     ConnectivityResult connectivityResult =
+//         await (Connectivity().checkConnectivity());
+//     if (connectivityResult == ConnectivityResult.mobile) {
+//       return true;
+//     } else if (connectivityResult == ConnectivityResult.wifi) {
+//       return true;
+//     }
+//     return false;
+//   }
+// }

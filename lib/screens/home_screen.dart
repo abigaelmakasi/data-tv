1b683e8947262513b9d1a3f39b6bdb826bc32d6c
abigaelmakasi.ic@agromwinda.com <abigaelmakasi.ic@agromwinda.com>
2023-02-28 08:57:46 +0100

    ajout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,64 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-=======
 import 'package:data_tv/screens/video_widget';
->>>>>>> main
+import 'package:video_player/video_player.dart';
+const Dblack = Colors.black;
+
+void main() => runApp(VideoApp());
+
+class VideoApp extends StatefulWidget {
+  @override
+  _VideoAppState createState() => _VideoAppState();
+}
+
+class _VideoAppState extends State<VideoApp> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video Demo',
+      home: Scaffold(
+        body: Center(
+          child: _controller.value.isInitialized
+              ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                )
+              : Container(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _controller.value.isPlaying
+                  ? _controller.pause()
+                  : _controller.play();
+            });
+          },
+          child: Icon(
+            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,115 +68,31 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-<<<<<<< HEAD
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text("data tv"),
-          ),
-          body: Column(
-            children: [
-              Row(
-                children: [
-                  Image.asset(
-                    "assets/images/img2.PNG",
-                    width: 100,
-                    height: 100,
-                  ),
-                  const Expanded(child: Text(
-                    ("bienvenue sur notre chaine data dhggvdfjfkljdfkl;jjgfj;lk "),
-                    style: TextStyle(
-                      fontSize: 14,
-                    ),
-                  ),)
-                 
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset(
-                    "assets/images/img2.PNG",
-                    width: 100,
-                    height: 100,
-                  ),
-                  const Text(
-                    ("l'arriver du pape dens la ville de kinshasa s")
-                
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset(
-                    "assets/images/img2.PNG",
-                    width: 100,
-                    height: 100,
-                  ),
-                  const Text("l'eglise catholique refuse les mariage")
-
-                                  
-                ],
-
-          
-              ),
-              Row(
-                children: [
-                  Image.asset(
-                    "assets/images/img2.PNG",
-                    width: 100,
-                    height: 100,
-                  ),
-                  const Text("MERCI MAMAN FOR ALL")
-
-                                  
-                ],
-
-          
-              ),
-              
-              Center(
-                child: ElevatedButton(
-                    onPressed:()=> print("click btn"),
-                    child:const Text("afficher le planning")
-                  ),
-        
-              )
-              
-            ],
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            selectedItemColor: Colors.pink,
-            unselectedItemColor: Colors.black,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: ("accuiel")
-                ),
-                BottomNavigationBarItem(
-                icon: Icon(Icons.calendar_month),
-                label: ("planing")
-                ),
-                BottomNavigationBarItem(
-                icon: Icon(Icons.add),
-                label: ("ajout")
-                ),
-            ],
-            
-            
-          )
-          ),
-    );
-  }
-=======
   late int pageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("data tv"),
+          backgroundColor: Dblack,
           elevation: 0.0,
+          leading:IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                  size: 30,
+                )), 
+        
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search_rounded,
+                  color: Colors.blue,
+                  size: 30,
+                )),
+          ],
         ),
         body: SingleChildScrollView(
           child: Padding(
@@ -143,6 +115,42 @@ class _HomeScreenState extends State<HomeScreen> {
                           "La future developpeuse Abigael Makasi abeli likolo ya Gradle. Mama ehhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
                       image: "assets/images/pape.jpg",
                     ),
+                    infosItem(
+                      title:
+                          "Visite du Pape Francois I a Kinshasa = RDC, il a ete recu par son excellence Fatshi Beeeeeeeeeeeeeeeee",
+                      image: "assets/images/pape.jpg",
+                    ),
+                    infosItem(
+                      title:
+                          "La future developpeuse Abigael Makasi abeli likolo ya Gradle. Mama ehhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
+                      image: "assets/images/pape.jpg",
+                    ),
+                    infosItem(
+                      title:
+                          "Visite du Pape Francois I a Kinshasa = RDC, il a ete recu par son excellence Fatshi Beeeeeeeeeeeeeeeee",
+                      image: "assets/images/pape.jpg",
+                    ),
+                    infosItem(
+                      title:
+                          "je suis la mamamn d'un magnifique petite garcons adorable",
+                      image: "assets/images/pape.jpg",
+                    ),
+                    infosItem(
+                      title:
+                          "je suis la mamamn d'un magnifique petite garcons adorable",
+                      image: "assets/images/pape.jpg",
+                    ),
+                    infosItem(
+                      title:
+                          "je suis la mamamn d'un magnifique petite garcons adorable",
+                      image: "assets/images/pape.jpg",
+                    ),
+                    infosItem(
+                      title:
+                          "je suis la mamamn d'un magnifique petite garcons adorable",
+                      image: "assets/images/pape.jpg",
+                    ),
+
                     // s
                     // const SizedBox(
                     //   height: 25,
@@ -255,7 +263,10 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-Widget infosItem({required String title, required String image, }) {
+Widget infosItem({
+  required String title,
+  required String image,
+}) {
   return Container(
     margin: const EdgeInsets.symmetric(vertical: 5),
     child: Row(
@@ -283,5 +294,4 @@ Widget infosItem({required String title, required String image, }) {
       ],
     ),
   );
->>>>>>> main
 }
